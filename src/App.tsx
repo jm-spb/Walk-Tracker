74b3feca351e956892515351a4dfb9238872392a
ivan <tuchel.ivan@gmail.com>
2021-12-22 23:24:11 +0300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h1>Walk Tracker</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus numquam quaerat
+      veniam libero magnam distinctio aut, maiores rem, tempora ab natus cupiditate
+      praesentium dicta iste error accusantium aperiam quis sequi rerum officia expedita
+      esse? Veritatis nulla eum ipsum ratione! Doloremque, quos sed ab dignissimos eveniet
+      tenetur blanditiis praesentium incidunt, voluptates, asperiores autem voluptate
+      nostrum quas! Architecto, sunt distinctio dolorem recusandae doloremque fugit
+      tempore assumenda suscipit cupiditate obcaecati, neque beatae, ad provident animi!
+      Eos officia aliquam sunt architecto placeat quis eveniet molestiae. Saepe ratione
+      iusto cum magnam doloremque aliquam, beatae, molestiae explicabo impedit minima sint
+      aspernatur voluptatum id. Aspernatur, labore facere.
+      <h1>Walk Tracker</h1>
+    </p>
+  </div>
+);
 
 export default App;
