a04c9c2b5025cafa14adf1bec6bf7d0a105952a6
ivan <tuchel.ivan@gmail.com>
2022-01-03 19:24:35 +0300

    add types & mapConfig

---
+++ src/mapConfig.ts
@@ -0,0 +1,5 @@
+export const clientID = process.env.REACT_APP_CLIENT_ID as string;
+export const clientSecret = process.env.REACT_APP_CLIENT_SECRET as string;
+export const refreshToken = process.env.REACT_APP_REFRESH_TOKEN as string;
+export const authLink = 'https://www.strava.com/oauth/token';
+export const activitiesLink = 'https://www.strava.com/api/v3/athlete/activities';
---
+++ src/types/index.ts
@@ -0,0 +1,33 @@
+export interface IFetchStravaData {
+  (
+    clientID: string,
+    clientSecret: string,
+    refreshToken: string,
+    authLink: string,
+    activitiesLink: string,
+  ): any;
+}
+
+export interface IRouteRenderData {
+  name: string;
+  distance: number;
+  average_speed: number;
+  max_speed: number;
+  moving_time: number;
+  coords: number[][];
+}
+
+interface IMapPolyline {
+  id: string;
+  summary_polyline: string;
+  resource_state: number;
+}
+
+export interface IRouteResponseData {
+  name: string;
+  distance: number;
+  average_speed: number;
+  max_speed: number;
+  moving_time: number;
+  map: IMapPolyline;
+}
