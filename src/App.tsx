b4575ae044224d62a440cc2d5e2d4dbcf8155486
ivan <tuchel.ivan@gmail.com>
2022-01-07 16:39:30 +0300

    connect redux-store with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,72 +1,34 @@
 import React from 'react';
-import polyline from '@mapbox/polyline';
+import { RootStateOrAny, useDispatch, useSelector } from 'react-redux';
 
 import './App.scss';
 
-import fetchStravaData from './api/fetchStravaData';
 import RoutesMap from './components';
+import { fetchRoutes } from './store/trackerSlice';
 
-import { IRouteRenderData, IRouteResponseData } from './types';
-import {
-  clientID,
-  clientSecret,
-  refreshToken,
-  authLink,
-  activitiesLink,
-} from './mapConfig';
+// import { IRouteRenderData, IRouteResponseData } from './types';
+// import {
+//   clientID,
+//   clientSecret,
+//   refreshToken,
+//   authLink,
+//   activitiesLink,
+// } from './mapConfig';
 
 const App = (): JSX.Element => {
-  const [routeData, setRouteData] = React.useState<IRouteRenderData[]>([
-    {
-      name: '',
-      distance: 0,
-      average_speed: 0,
-      max_speed: 0,
-      moving_time: 0,
-      coords: [[0, 0]],
-    },
-  ]);
+  const dispatch = useDispatch();
 
   React.useEffect(() => {
-    async function fetchAndSetRouteData() {
-      const responseData = await fetchStravaData(
-        clientID,
-        clientSecret,
-        refreshToken,
-        authLink,
-        activitiesLink,
-      );
+    dispatch(fetchRoutes());
+  }, [dispatch]);
 
-      const formatedData = responseData.data?.map(
-        ({
-          name,
-          distance,
-          average_speed,
-          max_speed,
-          moving_time,
-          map: { summary_polyline },
-        }: IRouteResponseData) => ({
-          name,
-          distance,
-          average_speed,
-          max_speed,
-          moving_time,
-          coords: polyline.decode(summary_polyline).map((coord) => coord.reverse()),
-        }),
-      );
-
-      setRouteData(formatedData);
-    }
-
-    fetchAndSetRouteData();
-  }, []);
-
-  console.log(routeData);
+  const { status } = useSelector((state: RootStateOrAny) => state);
+  console.log(status);
 
   return (
     <div className="App">
-      {routeData[0].distance ? (
-        <RoutesMap data={routeData} />
+      {status === 'fulfilled' ? (
+        <RoutesMap />
       ) : (
         <div>
           <h1>FETCHING DATA</h1>
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,30 +2,24 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import React from 'react';
 import mapboxgl from 'mapbox-gl';
-
-// import mapboxgldist from 'mapbox-gl/dist/mapbox-gl';
-// import MapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker';
+import { RootStateOrAny, useSelector } from 'react-redux';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
-import { IRoutesProps } from '../types';
+// import { IRoutesProps } from '../types';
 import getRandomColor from '../scripts/randomColor';
 import { mapboxToken } from '../mapConfig';
 
 mapboxgl.accessToken = mapboxToken;
 
-// mapboxgl.workerClass = MapboxWorker;
-
 (mapboxgl as any).workerClass =
   require('worker-loader!mapbox-gl/dist/mapbox-gl-csp-worker').default;
 
-const RoutesMap = ({ data }: IRoutesProps): JSX.Element => {
+const RoutesMap = (): JSX.Element => {
   const mapContainerRef = React.useRef(null);
+  const { routes } = useSelector((state: RootStateOrAny) => state);
 
   React.useEffect(() => {
-    // if (data[0].distance) {
-    // if (mapContainerRef.current) {
-    // console.log(data[0].distance);
     const map = new mapboxgl.Map({
       container: mapContainerRef.current as unknown as HTMLElement,
       style: 'mapbox://styles/mapbox/light-v10',
@@ -36,7 +30,7 @@ const RoutesMap = ({ data }: IRoutesProps): JSX.Element => {
     // add navigation control (the +/- zoom buttons)
     map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
 
-    const mapFeatures: any = data.map((route) => ({
+    const mapFeatures: any = routes.map((route: any) => ({
       type: 'Feature',
       properties: {
         color: getRandomColor(5),
@@ -66,10 +60,6 @@ const RoutesMap = ({ data }: IRoutesProps): JSX.Element => {
         },
       });
     });
-
-    // clean up on unmount
-    // return () => map.remove();
-    // }
   }, []);
 
   return <div className="map" ref={mapContainerRef} />;
