73f1d690c397c0c99290eeb13ff3622db7f1ef0c
ivan <tuchel.ivan@gmail.com>
2022-02-04 21:34:04 +0300

    App: add Offline component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,26 @@ import './App.scss';
 
 import RoutesMap from './components/RoutesMap';
 import Spinner from './components/Spinner';
+import OfflineMsg from './components/OfflineMsg';
+
 import { useAppDispatch, useAppSelector } from './hooks/redux';
 import { fetchRoutes } from './store/trackerSlice';
 
 const App = (): JSX.Element => {
+  // Check internet connection
+  if (!window.navigator.onLine) return <OfflineMsg />;
+
   const dispatch = useAppDispatch();
 
   React.useEffect(() => {
     dispatch(fetchRoutes());
   }, [dispatch]);
 
-  const { status } = useAppSelector((state) => state);
-  console.log(status);
+  const { status, error } = useAppSelector((state) => state);
+  console.log(`status: ${status}`);
+  console.log(`error: ${error}`);
+
+  // if (error) return
 
   return (
     <div className="walk-tracker">
