498c5bb21a791ebdcfa6b535c8cb8e000b75f125
ivan <tuchel.ivan@gmail.com>
2022-02-04 21:33:08 +0300

    create & config OfflineMsg component

---
+++ src/components/OfflineMsg/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import no_internet from '../../assets/no_internet.webp';
+
+const ErrorMsg = () => (
+  <div className="walk-tracker">
+    <div className="walk-tracker__offline">
+      <img src={no_internet} alt="no internet connection" />
+    </div>
+  </div>
+);
+
+export default ErrorMsg;
